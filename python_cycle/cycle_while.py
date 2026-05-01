539a5a32cad929ae4fc2fd75c434c32a7a835787
Kiryuha <arm2402@yandex.ru>
2026-05-01 22:13:30 +0500

    Cycle

---
+++ python_cycle/cycle_while.py
@@ -0,0 +1,7 @@
+invitations_left = 10
+
+while invitations_left > 0:
+    # Раздача продолжается, пока есть приглашение
+    print('Было вручено одно приглашение')
+    invitations_left -= 1 # Уменьшаем кол-во приглашений на 1
+    print('Осталось приглашений: ', invitations_left)
